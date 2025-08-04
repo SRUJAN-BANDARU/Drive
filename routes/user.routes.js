a8a6d7cf08594f3fe7b7a39fda87f57f80f32b8a
SRUJAN-BANDARU <bandarusrujan@gmail.com>
2025-08-04 12:19:47 +0530

    Added basic validation

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,19 +1,39 @@
 const express = require("express");
-
 const router = express.Router();
+const { body, validationResult } = require('express-validator');
 
 
 router.get("/test", (req, res) =>{
     res.send("Testing route");
 })
 
-router.get('/register', (req, res) =>{
+/*
+    email
+        'a@a.com'
+        '    '
+*/
+
+router.get('/register',(req, res) =>{
     res.render('../views/register')
 })
 
-router.post('/register', (req, res) =>{
-    console.log(req.body);
-    res.send("User Registered Successfully");
+
+router.post('/register',
+    body('email').trim().isEmail().isLength({min : 13}),
+    body('password').trim().isLength({min : 5}),
+    body('username').trim().isLength({min : 3}),
+    (req, res) =>{
+
+        const errors = validationResult(req);
+        
+        if(!errors.isEmpty()){
+            return res.status(400).json({
+                errors : errors.array(),
+                message : 'Invalid Data'
+            })
+        }
+        
+        res.send("User Registered Successfully");
 })
 
 module.exports = router;
