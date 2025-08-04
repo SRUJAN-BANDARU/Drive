4a0b120eb8e1463d2ee68b8e6e5745184f963687
SRUJAN-BANDARU <bandarusrujan@gmail.com>
2025-08-04 21:40:11 +0530

    Added Login based authentication using jwt

--- app.js
+++ app.js
@@ -1,8 +1,15 @@
 const express = require('express');
 const userRouter = require("./routes/user.routes");
+const dotenv = require('dotenv');
+const connectToDB = require('./config/db');
+const cookieParser = require('cookie-parser');
+dotenv.config();
+
+connectToDB();
 
 const app = express();
 
+app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({extended : true}));
 
--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,17 +1,16 @@
 const express = require("express");
 const router = express.Router();
 const { body, validationResult } = require('express-validator');
+const userModel = require('../models/user.model');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const user = require("../models/user.model");
 
 
 router.get("/test", (req, res) =>{
     res.send("Testing route");
 })
 
-/*
-    email
-        'a@a.com'
-        '    '
-*/
 
 router.get('/register',(req, res) =>{
     res.render('../views/register')
@@ -22,7 +21,7 @@ router.post('/register',
     body('email').trim().isEmail().isLength({min : 13}),
     body('password').trim().isLength({min : 5}),
     body('username').trim().isLength({min : 3}),
-    (req, res) =>{
+    async (req, res) =>{
 
         const errors = validationResult(req);
         
@@ -33,7 +32,70 @@ router.post('/register',
             })
         }
         
-        res.send("User Registered Successfully");
+        const {username, email, password} = req.body;
+
+        const hashedPass = await bcrypt.hash(password, 10);
+
+        const newUser = await userModel.create({
+            username : username,
+            email : email,
+            password : hashedPass
+        })
+        res.json(newUser);
 })
+
+router.get('/login', (req, res) =>{
+    res.render('login');
+})
+
+router.post('/login', 
+    body('email').trim().isEmail().isLength({min : 13}),
+    body('password').trim().isLength({min : 5})
+    ,
+    async(req, res) =>{
+        const errors = validationResult(req);
+
+        if(!errors.isEmpty()){
+            return res.status(400).json({
+                errors : errors.array(),
+                message : "Invalid Data"
+            })
+        }
+
+        const {email, password} = req.body;
+
+        const user = await userModel.findOne({
+            email : email
+        })
+
+        if(!user){
+            return res.status(400).json({
+                message : 'Username or password is incorrect'
+            })
+        }
+
+        const isMatch = await bcrypt.compare(password, user.password);
+        
+        if(!isMatch){
+            return res.status(400).json({
+                message : "Username or password is incorrect"
+            })
+        }
+
+        const token = jwt.sign({
+            userId : user._id,
+            username : user.username,
+            email : user.email
+        },
+            process.env.JWT_SECRET,
+        )
+
+        res.cookie('token', token);
+
+        res.send("Logged In");
+    }
+)
+
+
 
 module.exports = router;
